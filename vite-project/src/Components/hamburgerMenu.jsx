5e0e8898ed8cdb47478f6c51e0dbc80b9bebfd19
Zajahri <zajahrisolar10@gmail.com>
2024-07-29 00:32:25 +0800

    x button

--- vite-project/src/Components/hamburgerMenu.jsx
+++ vite-project/src/Components/hamburgerMenu.jsx
@@ -1,10 +1,11 @@
 
 import { Link } from "react-scroll";
+import { TiDeleteOutline } from "react-icons/ti";
 // eslint-disable-next-line react/prop-types
 export const HamburgerMenu = ({ close }) => {
   return (
     <div className="z-50 text-white overflow-hidden bg-slate-800 absolute h-[100vh] left-0 right-0 ">
-      <div className="flex flex-col items-center w-screen h-screen ">
+      <div className="relative flex flex-col items-center w-screen h-screen ">
         <ul className="mt-20 text-center">
         <li className="text-xl py-5">
             <Link
@@ -51,8 +52,8 @@ export const HamburgerMenu = ({ close }) => {
             </Link>
           </li>
         </ul>
-        <button className="text-3xl mt-52" onClick={close}>
-          x
+        <button className="text-3xl absolute bottom-[120px]" onClick={close}>
+        <TiDeleteOutline />
         </button>
       </div>
     </div>
