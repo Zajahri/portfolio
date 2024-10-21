3110dd3d71e8d048ab0b865fda8209643c46efc4
Zajahri <zajahrisolar10@gmail.com>
2024-10-21 22:22:55 +0800

    .

--- vite-project/src/Components/Footer.jsx
+++ vite-project/src/Components/Footer.jsx
@@ -5,8 +5,8 @@ export const Footer = () => {
     <div className='text-white h-auto font-["oswald"] '>
         <hr />
     <div className='  flex flex-col justify-center items-center py-5'>
-         <p className=''>© 2024 Zajahri F. Solar. All rights reserved.</p>
-         <p className=' text-sm'>Email: <a href="mailto:zajahrisolar10@gmail.com">zajahrisolar10@gmail.com</a></p>
+         <p className=' font-light'>© 2024 Zajahri F. Solar. All rights reserved.</p>
+         <p className='font-light '>Email: <a href="mailto:zajahrisolar10@gmail.com">zajahrisolar10@gmail.com</a></p>
     </div>
     </div>
   )
